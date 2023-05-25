ef99706677aef8cc7450573930fab097458dbf35
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2023-05-24 21:52:50 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio03</title>
+</head>
+<body>
+    <?php
+    $n1 = 10;
+    $n2 = 5;
+    $n3 = 2;
+    $n4 = 3;
+    $n5 = 8;
+
+    echo "($n1+$n2)* $n3 / $n4 - $n5 = ".($n1+$n2)*$n3/$n4-$n5 ;
+    ?>
+</body>
+</html>
